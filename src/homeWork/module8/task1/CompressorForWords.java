2a6d6658308a4a6ce8c4760dd19db70b26463d62
Volodymyr Silitskyi <volodymyr_silitskyi@epam.com>
2018-06-14 17:13:06 +0300

    Adding of changes for module 8, task 1

--- src/homeWork/module8/task1/CompressorForWords.java
+++ src/homeWork/module8/task1/CompressorForWords.java
@@ -11,11 +11,11 @@ public class CompressorForWords {
         String stringForCompress = "h2elloooooooooooo2";
         String stringForUncompress = "hel2o9o9o9o9o2";
 
-        compressString(stringForCompress);
-        uncompressString(stringForUncompress);
+        System.out.println(compressString(stringForCompress));
+        System.out.println(uncompressString(stringForUncompress));
     }
 
-    public static void compressString(String stringForCompress) {
+    public static String compressString(String stringForCompress) {
         StringBuilder newString = new StringBuilder();
         int numberOfRepeatedValue = 1;
         char previousCharacter = stringForCompress.charAt(0);
@@ -42,28 +42,35 @@ public class CompressorForWords {
         } else {
             newString.append(previousCharacter);
         }
-        System.out.println(newString.toString());
+//        System.out.println(newString.toString());
+        return stringForCompress;
     }
 
-    public static void uncompressString(String stringForUncompress) {
+    public static String uncompressString (String stringForUncompress){
+        final char MIN_NUMBER = 47;
+        final char MAX_NUMBER = 58;
         StringBuilder newString = new StringBuilder();
         int numberOfRepeatedValue = 0;
         char previousCharacter = stringForUncompress.charAt(0);
-
-        for (int i = 1; i < stringForUncompress.length(); i++) {
-            if (stringForUncompress.charAt(i) > 47 && stringForUncompress.charAt(i) < 58) {
-                numberOfRepeatedValue = Integer.parseInt(String.valueOf(stringForUncompress.charAt(i)));
-                for (int j = 1; j < numberOfRepeatedValue; j++) {
-                    newString.append("" + previousCharacter);
+        try {
+            for (int i = 1; i < stringForUncompress.length(); i++) {
+                if (stringForUncompress.charAt(i) > MIN_NUMBER && stringForUncompress.charAt(i) < MAX_NUMBER) {
+                    numberOfRepeatedValue = Integer.parseInt(String.valueOf(stringForUncompress.charAt(i)));
+                    for (int j = 1; j < numberOfRepeatedValue; j++) {
+                        newString.append("" + previousCharacter);
+                    }
+                } else {
+                    newString.append(previousCharacter);
+                    previousCharacter = stringForUncompress.charAt(i);
                 }
-            } else {
-                newString.append(previousCharacter);
-                previousCharacter = stringForUncompress.charAt(i);
             }
-        }
-        newString.append(previousCharacter);
+            newString.append(previousCharacter);
 
-        System.out.println(newString.toString());
+            System.out.println(newString.toString());
+        } catch (Exception e) {
+            System.out.println("Isn't correct value");
+        }
+        return stringForUncompress;
     }
 
 }
