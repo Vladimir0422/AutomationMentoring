6b5bc518db33e8809afa2a71422e146ab799d17e
Volodymyr Silitskyi <volodymyr_silitskyi@epam.com>
2018-07-30 16:24:14 +0300

    Adding of module 8, task 1 Compressor for words

--- src/homeWork/module7/task1/DisplayCurrentTime.java
+++ src/homeWork/module7/task1/DisplayCurrentTime.java
@@ -1,5 +1,7 @@
 package homeWork.module7.task1;
 
+import com.sun.security.ntlm.Client;
+
 /**
  * Created by : Volodymyr_Silitskyi
  * Created at : 6/11/2018
@@ -7,18 +9,18 @@ package homeWork.module7.task1;
 
 
 public class DisplayCurrentTime {
-    public static void main(String[] args) throws InterruptedException {
-        DateTime server1 = new DateTime();
-        DateTime server2 = new DateTime();
-        Client client1 = new Client();
-        Client client2 = new Client();
-
-        server1.addObserver(client1);
-        server2.addObserver(client2);
-        for (int i = 0; i < 5; i++) {
-            server1.notifyObservers();
-            Thread.sleep(2000);
-            server2.notifyObservers();
-        }
-    }
+//    public static void main(String[] args) throws InterruptedException {
+//        DateTime server1 = new DateTime();
+//        DateTime server2 = new DateTime();
+//        Client client1 = new Client();
+//        Client client2 = new Client();
+//
+//        server1.addObserver(client1);
+//        server2.addObserver(client2);
+//        for (int i = 0; i < 5; i++) {
+//            server1.notifyObservers();
+//            Thread.sleep(2000);
+//            server2.notifyObservers();
+//        }
+//    }
 }
