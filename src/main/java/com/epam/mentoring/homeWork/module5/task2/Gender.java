87651b2d4e8494ea434dc2c54ef27507e7dbb912
Volodymyr Silitskyi <volodymyr_silitskyi@epam.com>
2018-09-25 14:57:14 +0300

    Adding of logging

---
+++ src/main/java/com/epam/mentoring/homeWork/module5/task2/Gender.java
@@ -0,0 +1,11 @@
+package com.epam.mentoring.homeWork.module5.task2;
+
+/**
+ * Created by : Volodymyr_Silitskyi
+ * Created at : 9/3/2018
+ */
+
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/epam/mentoring/homeWork/module5/task2/Hobby.java
@@ -0,0 +1,11 @@
+package com.epam.mentoring.homeWork.module5.task2;
+
+/**
+ * Created by : Volodymyr_Silitskyi
+ * Created at : 9/11/2018
+ */
+
+
+public enum Hobby {
+    DANCING, SWIMMING, GAMING
+}
